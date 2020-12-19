b4ada13be4b68da31c35236a2de5f0971652f48e
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-12-19 18:17:26 +0700

    Initial app

---
+++ components/Welcome.js
@@ -0,0 +1,6 @@
+const Welcome = () => (
+  <h2 style={{ textAlign: "center" }}>
+    Welcome to Don't Be A Junior community
+  </h2>
+);
+export default Welcome;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import Welcome from "../components/Welcome";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <Welcome />
+  </div>
+);
+
+export default Home;
